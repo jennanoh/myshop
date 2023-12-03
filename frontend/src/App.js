08221a9b2155de669910744eef8543cf74e887e8
jennanoh <jennanoh28@gmail.com>
2023-12-03 13:21:42 -0500

    React project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+//jsx file (js file with html in a function)
+//html must all be within one <>
+
+/*function App() {
+  return (
+    <>
+     
+    </>
+  );
+}*/
+
+//es7 (standards for js syntax) has a different way to write a function
+const App = () => {
+  return(
+    <>
+    <h1>Welcome to my shop!</h1>
+    </>
+  );
+}
+
+
+export default App;
