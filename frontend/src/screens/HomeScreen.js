aedb543c49225d5068a0cbbb35849b9aef2a47e1
jennanoh <jennanoh28@gmail.com>
2024-01-04 17:03:39 -0500

    homescreen columns and products component added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,8 +5,17 @@ import Products from '../componets/Products'
 const HomeScreen = () => {
   return (
     <>
-      <h1>
-      </h1>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map(p => (
+            <Col sm={12} md={16} lg={4} xl={3}>
+              <Products products={p}/>
+            </Col>
+          ))
+          //map function lets you loop through data.
+          //pass the products parameter to the Products component
+        }
+      </Row>
     </>
   )
 }
