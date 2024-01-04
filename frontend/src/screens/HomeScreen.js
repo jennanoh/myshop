f2124072e3d58dbf822f2ed06a8f242dbd4f4196
jennanoh <jennanoh28@gmail.com>
2024-01-04 16:04:49 -0500

    home screen created - will display products

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import products from '../products'
+import Products from '../componets/Products'
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>
+      </h1>
+    </>
+  )
+}
+
+export default HomeScreen
