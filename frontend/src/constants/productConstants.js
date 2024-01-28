31610a938ee0c510299924fae0a416ed28c28317
jennanoh <jennanoh28@gmail.com>
2024-01-28 17:19:13 -0500

    configurestore, make dispatch codes

---
+++ frontend/src/constants/productConstants.js
@@ -0,0 +1,8 @@
+//constants are usually in uppercase - means they dont change
+//these are dispatch codes
+//actions will call the API and dispatch a code to write the array of data to the store
+//reducers will read the dispatch code
+export const PRODUCT_LIST_REQUEST = "PRODUCT_LIST_REQUEST"
+export const PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS"
+export const PRODUCT_LIST_FAIL = "PRODUCT_LIST_FAIL"
+
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,13 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit'
+import { productListReducer } from './reducers/productReducers'
 //register the reducers allowed to write data to the store
+//use combineReducers in the case of multiple reducers
 
+const rootReducer = combineReducers({
+  productList: productListReducer
+})
 const store = configureStore({
-  reducer: {},
+  reducer: rootReducer,
   preloadedState:{}
 })
 
