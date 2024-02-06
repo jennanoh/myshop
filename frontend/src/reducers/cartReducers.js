4b20c8b37dae8925c12d60c60feae4db0c6d1102
jennanoh <jennanoh28@gmail.com>
2024-02-05 21:04:39 -0500

    shipping page, action and reducers

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM,CART_REMOVE_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM,CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
 
 export const cartReducer = (state = {cartItems:[]}, action) => {
   switch (action.type){
@@ -25,7 +25,19 @@ export const cartReducer = (state = {cartItems:[]}, action) => {
         cartItems: state.cartItems.filter(x => x.product !== action.payload)
         //filter to anything that didnt exist in the payload aka the items you want to remove
       }
+
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return{
+        ...state,
+        shippingAddress: action.payload
+      }
+    case CART_SAVE_PAYMENT_METHOD:
+      return{
+        ...state,
+        paymentMethod: action.payload
+      } //now the state has cartItems, shippingAddress and paymentMethod
     default:
       return state
   }
 }
+
