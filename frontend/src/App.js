7ca15c191fb475c01ca35e2f79e864abd428e7ef
jennanoh <jennanoh28@gmail.com>
2024-02-05 21:05:45 -0500

    shipping info and user info retrived from browser storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import Header from "./componets/Header";
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import ShippingScreen from './screens/Shipping';
+
 
 /*function App() {
   return (
@@ -28,6 +31,8 @@ const App = () => {
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/shipping" element={<ShippingScreen/>} />
           </Routes>
         </Container>
       </main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,36 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 //register the reducers allowed to write data to the store
 //use combineReducers in the case of multiple reducers
 
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer
 })
 //we need to update the preloaded State of the cart by retrieving data back from local browser storage
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems') 
+  ? JSON.parse(localStorage.getItem('cartItems')) 
+  : []
 //put this preloaded state to a separate variable as good practice
 //in case there might be diff preloaded states for diff reducers
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod') ? JSON.parse(localStorage.getItem('paymentMethod')) : {}
+
 const initialState = {
-  cart: {cartItems: cartItemsFromStorage}
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+    paymentMethod: paymentMethodFromStorage
+    },
+  userLogin: {userInfo: userInfoFromStorage},
 }
 const store = configureStore({
   reducer: rootReducer,
