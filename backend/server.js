014dff33323bde85d08f40599bab496f2cd8f330
jennanoh <jennanoh28@gmail.com>
2024-02-21 12:47:32 -0500

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRoutes from './routes/productRoutes.js'
 import errorHandler from './middleware/errorMiddleware.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import path from 'path'
 
 const app = express()
 dotenv.config()
@@ -18,6 +19,19 @@ app.use('/api/orders', orderRoutes)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID))
 
+const __dirname = path.resolve() //directory
+if (process.env.NODE_ENV === 'production'){ //if in production mode
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html'))
+  }) //production mode will only run backend - cannot do conccurently
+  //use express to the static folder in frontend
+}// '*' means whatever url is returned
+//find the index.html in the frontend build folder
+
 app.use(errorHandler)
-app.listen(5000, console.log('Server is running on port 5000'))
+// app.listen(5000, console.log('Server is running on port 5000'))
 //this tells you that the backend is running
+//change for the purpose of deployment - port 5000 is only guarenteed locally
+const port = process.env.PORT
+app.listen(port, console.log(`Server is running on port ${port}`))
